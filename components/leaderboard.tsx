4b4b31f980d6e62220dad73f02ad7af3b5ef91f2
ichenglin <fireclaws9@gmail.com>
2023-05-18 00:34:22 +0800

    Added Leaderboard Priority

--- components/leaderboard.tsx
+++ components/leaderboard.tsx
@@ -14,7 +14,13 @@ const Leaderboard: NextPageLayout<{leaderboard: Player[]}> = (props) => {
 					<th>Deaths</th>
 					<th>Latency</th>
 				</tr>
-				{props.leaderboard.map((player_object, player_index) => {
+				{props.leaderboard.sort((player_a, player_b) => {
+					const data_a  = player_a.data_get().player_match;
+					const data_b  = player_b.data_get().player_match;
+					const score_a = (data_a.player_kills - data_a.player_deaths);
+					const score_b = (data_b.player_kills - data_b.player_deaths);
+					return score_b - score_a;
+				}).map((player_object, player_index) => {
 					const player_data = player_object.data_get();
 					return <tr key={player_index}>
 						<td data-team={player_data.player_team}>{player_data.player_username}</td>
